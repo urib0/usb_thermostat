ee7443b008e1850176dab3b71488b1c8d5e300c2
uribo <ur1b0.dev@gmail.com>
2023-05-28 17:05:47 +0900

    ssrと温度計をクラスにした

--- main.py
+++ main.py
@@ -8,40 +8,78 @@ import json
 import datetime as dt
 import serial
 import datetime
+from typing import Union
 
 DEBUG = False
 SSR_ON = True
 SSR_OFF = False
-SSR_RETRY = 3
 kP = 1
-kI = -3
+kI = 0
 kD = 0
 
-def ssr_controller(switch: bool):
-        for i in range(SSR_RETRY):
+class usb_comon_sense_controller():
+    def __init__(self,port,rate):
+        self.device = serial.Serial(port,rate,timeout=3)
+
+    def write(self,data: str) -> bool:
+        for i in range(3):
             try:
-                ssr = serial.Serial(conf["ssr"]["serial_port"],conf["ssr"]["serial_rate"],timeout=3)
-                if switch:
-                    ssr.write(b"1")
-                else:
-                    ssr.write(b"0")
-                ssr.close()
+                self.device.write(data.encode())
                 return True
             except Exception as e:
                 print(f"error:{e}")
         return False
 
-def thermocouple():
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-    filename = conf["logdir"] + "/" +\
-        conf["thermo"]["sensor_name"] + "/" +\
-        conf["thermo"]["sensor_name"] + "_" +\
-        timestamp + ".csv"
-    f = open(filename,"r")
-    lines = f.readlines()[-1:][0][:-1]
-    f.close
-    temp = float(lines.split(",")[1].split(";")[0].split("=")[1])/100
-    return temp
+    def read(self) -> str:
+        self.device.reset_input_buffer()
+        return self.device.readline().decode().replace("\n","")
+
+    def __del__(self):
+        self.device.close()
+
+class ssr_controller():
+    def __init__(self,port,rate):
+        try:
+            self.ssr = usb_comon_sense_controller(port,rate)
+        except Exception as e:
+            print(e)
+            exit(0)
+
+    def on(self) -> bool:
+        return self.ssr.write("1")
+
+    def off(self) -> bool:
+        return self.ssr.write("0")
+
+    def __del__(self):
+        self.off()
+
+class themocouple_controller():
+    def __init__(self,port,rate):
+        try:
+            self.thermo = usb_comon_sense_controller(port,rate)
+        except Exception as e:
+            print(e)
+            exit(0)
+
+    def read_temp(self) -> Union[float,None]:
+        raw = self.thermo.read()
+        raw = raw.split(";")[0]
+        if raw.split("=")[0] == "te":
+            return None
+        return int(raw.split("=")[1])/100
+
+#def thermocouple():
+#    timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+#    filename = conf["logdir"] + "/" +\
+#        conf["thermo"]["sensor_name"] + "/" +\
+#        conf["thermo"]["sensor_name"] + "_" +\
+#        timestamp + ".csv"
+#    f = open(filename,"r")
+#    lines = f.readlines()[-1:][0][:-1]
+#    f.close
+#    temp = float(lines.split(",")[1].split(";")[0].split("=")[1])/100
+#    return temp
 
 def conv(data):
     if data[0] in {"temp", "hum", "te"}:
@@ -59,14 +97,17 @@ def digit_alignment(num):
 f = open("./config.json", "r")
 conf = json.loads(f.read())
 f.close()
+ssr = ssr_controller(conf["ssr"]["serial_port"],conf["ssr"]["serial_rate"])
+thermo = themocouple_controller(conf["thermo"]["serial_port"],conf["thermo"]["serial_rate"])
+
 interval = conf["interval"]
 temp_target = conf["TEMP_TARGET"]
-temp_old = thermocouple()
+temp_old = thermo.read_temp()
 
 while True:
     try:
         #　熱電対読み込み
-        temp = thermocouple()
+        temp = thermo.read_temp()
 
         dt = interval
         P = temp_target-temp
@@ -78,15 +119,15 @@ while True:
         logger(f"interval:{round(interval,3)},ontime:{round(ontime,3)},offtime:{round(interval-ontime,3)},temp:{digit_alignment(temp)},tP:{digit_alignment(P*kP)},tI:{digit_alignment(I*kI)},tD:{digit_alignment(D*kD)}")
 
         if ontime<=0:
-            ssr_controller(SSR_OFF)
+            ssr.off()
             time.sleep(interval)
         elif ontime > interval:
-            ssr_controller(SSR_ON)
+            ssr.on()
             time.sleep(interval)
         else:
-            ssr_controller(SSR_ON)
+            ssr.on()
             time.sleep(ontime)
-            ssr_controller(SSR_OFF)
+            ssr.off()
             time.sleep(interval-int(ontime))
 
     except KeyboardInterrupt:
